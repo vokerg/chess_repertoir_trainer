184619dc137b45c51429bd079b8304b04115a855
vokerg <vokerg@gmail.com>
2026-05-25 19:04:03 +0200

    Register analysis API module

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import coursesModule from '../modules/courses/courses.routes';
 import trainingModule from '../modules/training/training.routes';
 import statsModule from '../modules/stats/stats.routes';
+import analysisModule from '../modules/analysis/analysis.routes';
 import importExportRoutes from './importExport';
 import externalAccountsRoutes from './externalAccounts';
 import swaggerRoutes from './swagger';
@@ -10,6 +11,7 @@ export default function registerRoutes(app: FastifyInstance): void {
   app.register(coursesModule);
   app.register(trainingModule);
   app.register(statsModule);
+  app.register(analysisModule);
   app.register(importExportRoutes);
   app.register(externalAccountsRoutes);
   app.register(swaggerRoutes);
