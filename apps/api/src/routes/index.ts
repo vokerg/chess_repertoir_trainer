7187dae7c609a9cc383789d71f4c75ed7ac319dd
vokerg <vokerg@gmail.com>
2026-05-25 12:10:35 +0200

    refactor(api): compose route modules by feature

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,15 +1,15 @@
 import { FastifyInstance } from 'fastify';
 import coursesModule from '../modules/courses/courses.routes';
-import trainingRoutes from './training';
-import statsRoutes from './stats';
+import trainingModule from '../modules/training/training.routes';
+import statsModule from '../modules/stats/stats.routes';
 import importExportRoutes from './importExport';
 import externalAccountsRoutes from './externalAccounts';
 import swaggerRoutes from './swagger';
 
 export default function registerRoutes(app: FastifyInstance): void {
   app.register(coursesModule);
-  app.register(trainingRoutes);
-  app.register(statsRoutes);
+  app.register(trainingModule);
+  app.register(statsModule);
   app.register(importExportRoutes);
   app.register(externalAccountsRoutes);
   app.register(swaggerRoutes);
