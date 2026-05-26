93f6f843ef12d584015817cc2a7b4abc2af08294
vokerg <vokerg@gmail.com>
2026-05-26 21:44:34 +0200

    Register imported games API module

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -3,6 +3,7 @@ import coursesModule from '../modules/courses/courses.routes';
 import trainingModule from '../modules/training/training.routes';
 import statsModule from '../modules/stats/stats.routes';
 import analysisModule from '../modules/analysis/analysis.routes';
+import importedGamesModule from '../modules/imported-games/imported-games.routes';
 import importExportRoutes from './importExport';
 import externalAccountsRoutes from './externalAccounts';
 import swaggerRoutes from './swagger';
@@ -12,6 +13,7 @@ export default function registerRoutes(app: FastifyInstance): void {
   app.register(trainingModule);
   app.register(statsModule);
   app.register(analysisModule);
+  app.register(importedGamesModule);
   app.register(importExportRoutes);
   app.register(externalAccountsRoutes);
   app.register(swaggerRoutes);
