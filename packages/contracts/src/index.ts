ad2f22a29a4f7c7977d7b9fa6eea6437a511bd5f
vokerg <vokerg@gmail.com>
2026-07-28 10:47:45 +0200

    RB-006: expose repertoire target contracts (#94)

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -8,5 +8,6 @@ export * from './lab';
 export * from './opening-struggles';
 export * from './opening-explorer';
 export * from './player-chess-profile';
+export * from './repertoire-target';
 export * from './jobs';
 export * from './rating-normalization';
