df0de361e9afea1ba53a9c415d321db20dd29481
vokerg <vokerg@gmail.com>
2026-07-29 08:00:26 +0200

    VT-202: add Study line hierarchy inputs

--- apps/web/src/app/features/library/components/study-line-list/study-line-list.component.ts
+++ apps/web/src/app/features/library/components/study-line-list/study-line-list.component.ts
@@ -1,7 +1,13 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LibraryLine } from '../../data-access/library.models';
-import { coverageLabel, lineStatus, masteryLabel, sideLabel, statusClass, statusLabel } from '../../helpers/library-line.helpers';
+import {
+  coverageLabel,
+  lineStatus,
+  masteryLabel,
+  sideLabel,
+  statusLabel,
+} from '../../helpers/library-line.helpers';
 
 @Component({
   selector: 'app-study-line-list',
@@ -12,6 +18,9 @@ import { coverageLabel, lineStatus, masteryLabel, sideLabel, statusClass, status
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudyLineListComponent {
+  readonly stepLabel = input.required<string>();
+  readonly title = input('Lines');
+  readonly subtitle = input('Review and select lines.');
   readonly lines = input.required<LibraryLine[]>();
   readonly selectedLineId = input<number | null>(null);
   readonly selectedLineIds = input<number[]>([]);
@@ -21,10 +30,10 @@ export class StudyLineListComponent {
   readonly toggleLine = output<number>();
   protected readonly lineStatus = lineStatus;
   protected readonly statusLabel = statusLabel;
-  protected readonly statusClass = statusClass;
   protected readonly sideLabel = sideLabel;
   protected readonly masteryLabel = masteryLabel;
   protected readonly coverageLabel = coverageLabel;
+
   protected isChecked(lineId: number): boolean {
     return this.selectedLineIds().includes(lineId);
   }
