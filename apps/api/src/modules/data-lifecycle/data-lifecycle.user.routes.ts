63125ed9c2d349a900b286087b57297caf54ff61
vokerg <vokerg@gmail.com>
2026-09-13 07:06:56 +0200

    chore(lifecycle): remove unused route schema import

--- apps/api/src/modules/data-lifecycle/data-lifecycle.user.routes.ts
+++ apps/api/src/modules/data-lifecycle/data-lifecycle.user.routes.ts
@@ -1,7 +1,6 @@
 import {
   dataLifecycleErrorResponseSchema,
   dataLifecycleExecuteRequestSchema,
-  dataLifecycleIdentityBlockedResponseSchema,
   dataLifecycleOperationResponseSchema,
   dataLifecyclePreviewResponseSchema,
   dataLifecycleReceiptStatusRequestSchema,
